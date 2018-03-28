1ab8211a716398f20fbc840a367eb5168c3a6cf7
yubing <29988076@qq.com>
2018-03-28 18:43:11 +0800

    ..

--- server/app.js
+++ server/app.js
@@ -8,7 +8,12 @@ const bunyan = require('bunyan')
 const mkdirp = require('mkdirp')
 const koaBunyan = require('koa-bunyan')
 const koaLogger = require('koa-bunyan-logger')
-
+const session = require("koa-session")
+const compose = require('koa-compose')
+const compress = require('koa-compress')
+const koaBody = require("koa-body");
+const debugModule = require('debug')
+const api = require('./api')
 
 async function start() {
     const isWin = /^win/.test(process.platform)
@@ -25,7 +30,7 @@ async function start() {
    
     let repliceSrc = logDir.replace(/(\\|\/)+$/, '') + (isWin ? '\\' : '/');
     //console.log(repliceSrc);
-    
+   
     mkdirp.sync(repliceSrc, function (err) {
         if (err) console.error("asdaaaaaaaaa")
         else console.log('pow!')
@@ -53,10 +58,8 @@ async function start() {
     app.use(koaBunyan(logger, { level: 'info' }))
     app.use(koaLogger(logger))
 
-
-
     app.use(async function subApp(ctx, next) {
-        console.log("_____"+ctx.url.split('/')[1]);
+        //console.log("_____"+ctx.url.split('/')[1]);
         
         ctx.state.subapp = ctx.url.split('/')[1];
         await next();
@@ -81,16 +84,54 @@ async function start() {
     
     const nuxtRender = koaConnect(nuxt.render)
     //console.log(nuxtRender);
-    console.log(ctx.state.subapp);
     app.use(async (ctx,next)=>{
         await next()
-        console.log(ctx.state.subapp);
+        //console.log("+_+_" + ctx.state.subapp);
         if (ctx.state.subapp !== consts.API){
-
+            ctx.status = 200;
+            //console.log(ctx.session);
+            ctx.req.session = ctx.session;
+            await nuxtRender(ctx)
         }
-        
-
     })
+
+    const SESSION_CONFIG = {
+        key: consts.SESS_KEY
+    }
+
+    app.use(session(SESSION_CONFIG,app))
+
+    app.use(async function responseTime(ctx,next) {
+        const t1 = Date.now();
+        await next()
+        const t2 = Date.now()
+        //console.log(Math.ceil(t2 - t1));
+        
+        ctx.response.set('X-Response-Time', Math.ceil(t2 - t1) + 'ms')
+    })
+
+    //数据压缩
+    app.use(compress({}))
+
+    // only search-index www subdomain
+    app.use(async function robots(ctx, next) {
+        await next()
+        if (ctx.hostname.slice(0, 3) !== 'www') {
+            ctx.response.set('X-Robots-Tag', 'noindex, nofollow')
+        }
+    })
+
+    app.use(koaBody())
+
+
+    app.use(async function (ctx, next) {
+        debug(ctx.method + ' ' + ctx.url)
+        await next()
+    })
+
+
+
+
 
 
 
